bc4c5f52aac97e38c93ab10b37efd44021fdd924
MahmudurRashid6604 <mahmudrashid6604@gmail.com>
2022-06-15 18:43:00 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+// Falsy
+// 0
+// ""
+// undefined
+// null
+// NaN
+// truthy
+// " ", "0", []
+
+
+let name = {};
+if(name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
