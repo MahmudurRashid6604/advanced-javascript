072312afe20335be7f77ff8e265091a41d6639e2
MahmudurRashid6604 <mahmudrashid6604@gmail.com>
2022-06-16 00:09:48 +0600

    pushing null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,17 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+};
+const premika = { name: "cute girl", phone:0170000 };
+console.log(premika.bf);
+
+const fun = undefined;
+console.log(fun);
+
+let ages = [2, 3, 4];
+console.log(ages[5]);
